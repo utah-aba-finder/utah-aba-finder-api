f7617b45e75de368dfc2a5f99ede1d1100f10b5d
Chee Lee <cheeleertr@gmail.com>
2024-08-26 15:46:50 -0500

    add serializer

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -7,25 +7,34 @@ class ProviderSerializer
         {
           id: provider.id,
           type: "provider",
+          # attributes need to be updated
           attributes: {
             "name": provider.name,
-            "website": provider.website,
-            "address": provider.address,
             "locations": provider.locations.map do |location| 
               {
               name: location.name,
+              address_1: location.address_1,
+              address_2: location.address_2,
+              city: location.city,
+              state: location.state,
+              zip: location.zip,
               phone: location.phone
               }
             end,
-            "phone": provider.phone,
+            "website": provider.website,
+            "address": provider.address,
             "email": provider.email,
-            "insurance": provider.insurance.map { |insurance| insurance.name},
-            "areas_served": provider.areas_served.map { |area| area.name},
             "cost": provider.cost,
-            "ages_served": provider.ages_served,
-            "waitlist": provider.waitlist,
+            "insurance": provider.insurance.map { |insurance| insurance.name},
+            "counties_served": provider.counties_served.map { |area| area.counties_served},
+            "min_age": provider.min_age,
+            "max_age": provider.max_age,
+            "waitlist_detail": provider.waitlist_detail,
             "telehealth_services": provider.telehealth_services,
-            "spanish_speakers": provider.spanish_speakers
+            "spanish_speakers": provider.spanish_speakers,
+            "at_home_services": provider.at_home_services,
+            "in_clinic_services": provider.in_clinic_services,
+            "logo": provider.logo
           }
         }
       end
