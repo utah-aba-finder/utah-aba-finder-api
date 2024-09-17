fbe618e9cd6030c94a8e0d2de24ac3307c91e0f6
Chee Lee <cheeleertr@gmail.com>
2024-09-17 12:06:55 -0500

    add client table with name and api_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_220609) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_17_165313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "api_key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_clients_on_api_key", unique: true
+    t.index ["name"], name: "index_clients_on_name", unique: true
+  end
 
   create_table "counties", force: :cascade do |t|
     t.bigint "provider_id", null: false
