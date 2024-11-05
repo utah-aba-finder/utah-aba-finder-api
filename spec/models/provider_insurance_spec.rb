9d46d5b18ab3974a170fbe7030ed2fa0f0eee133
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-11-05 15:04:44 -0500

    TDD provider_insurance unit test

--- spec/models/provider_insurance_spec.rb
+++ spec/models/provider_insurance_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe ProviderInsurance, type: :model do
     it { should belong_to(:provider) }
     it { should belong_to(:insurance) }
   end
+
+  describe '#initialize_provider_insurances' do
+    it 'creates a ProviderInsurance record with accepted: false for each insurance' do
+      insurance1 = Insurance.create!(name: "Insurance A")
+      insurance2 = Insurance.create!(name: "Insurance B")
+      insurance3 = Insurance.create!(name: "Insurance C")
+
+      provider = Provider.create!(name: "Test Provider")
+
+      expect(ProviderInsurance.count).to eq(0)
+
+      provider.initialize_provider_insurances
+
+      expect(ProviderInsurance.count).to eq(3)
+
+      ProviderInsurance.all.each do |provider_insurance|
+        expect(provider_insurance.provider_id).to eq(provider.id)
+        expect(provider_insurance.accepted).to be_false
+      end
+    end
+  end
 end
