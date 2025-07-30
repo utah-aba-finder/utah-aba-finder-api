8d87fbf92e8e6ad6669d2a74618860140302bf68
jordan williamson <williamsonjordan05@gmail.com>
2025-07-30 16:28:48 -0600

    Update password reset email to point to frontend application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Devise mailer configuration
-  config.action_mailer.default_url_options = { host: 'utah-aba-finder-api-c9d143f02ce8.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'utah-aba-finder.vercel.app' }
   
   # Email configuration
   config.action_mailer.delivery_method = :smtp
