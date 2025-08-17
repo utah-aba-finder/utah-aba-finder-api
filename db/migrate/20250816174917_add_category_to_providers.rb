b52569436cb033ec9b2c9f427d3612810b082b93
jordan williamson <williamsonjordan05@gmail.com>
2025-08-16 21:50:48 -0600

    Make category migration safe: only add column if it doesn't exist

--- db/migrate/20250816174917_add_category_to_providers.rb
+++ db/migrate/20250816174917_add_category_to_providers.rb
@@ -1,6 +1,8 @@
 class AddCategoryToProviders < ActiveRecord::Migration[7.1]
   def change
-    add_column :providers, :category, :string, default: 'aba_therapy'
-    add_index :providers, :category
+    unless column_exists?(:providers, :category)
+      add_column :providers, :category, :string, default: 'aba_therapy'
+      add_index :providers, :category
+    end
   end
 end 
